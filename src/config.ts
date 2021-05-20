1cab14b1189624a342a203cbfa03df4195359392
caquijano <carlosquijano98@gmail.com>
2021-05-20 12:18:40 -0500

    first commit3

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default{
-    //API_HOST: 'https://api-plantilla1.herokuapp.com',
-    API_HOST: 'http://localhost:4000',
+    API_HOST: 'https://api-plantilla1.herokuapp.com',
+    //API_HOST: 'http://localhost:4000',
     
 }
